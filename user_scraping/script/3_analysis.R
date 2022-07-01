ccaa47e7d125b643faf46a74822b68fb1082c255
Renbrooke <69281726+Renbrooke@users.noreply.github.com>
2022-06-30 22:59:46 -0400

    added sentiment analysis script

--- user_scraping/script/3_analysis.R
+++ user_scraping/script/3_analysis.R
@@ -3,11 +3,11 @@ GOALS FOR PRESENTATION:
 Descriptive summary of data:
   What were our source-subreddits (sub_list_reduced), and why?
   How did we collect the data?
-  
+      
   How many subreddits, users and posts in total? How many after reduction?
       We scraped 1,753,752 posts, but the subreddit list was not well defined. 
       After retroactively redefining it we are left with 73,496 posts by 136 
-      users, that posted in 3532 subreddits
+      users, that posted in 3532 subreddits 
   How many groups, and what percent of subreddits were we able to categorize?
 
 
@@ -69,6 +69,35 @@ plot <- data %>%
   coord_flip()  
 plot
 
+# Case Study: schadenfreude group of subreddits
+# 
+# Perhaps the best way to compare this is in terms of ranking:
+# ranking of sub_group count 
+# 
+# as compared to average overall ranking of subreddits:
+# #
+# # subreddit ranks (based on total subscribers, not posts; source: frontpagemetrics.com; 
+# in future research, we'd like to develop a more consistent comparison)
+# total number of subreddits as of 6/24/2022: 3,544,167
+# 
+# yesyesyesno : rank 704
+# winstupidprizes: rank 254
+# thatlookedexpensive: rank 799
+# publicfreakouts: rank 18,256
+# publicfreakoutsreborn: rank 11,514
+#actualpublicfreakouts: rank 1,063
+# instantregret: rank 1244
+#  instantkarma: rank 253
+#  robbersgettingfucked: 4,997
+# averageredditor: 6372
+# whatcouldgowrong: rank 267
+# nsfl__ 
+# pettyrevenge: rank 364 
+# sadcringe: 511
+# 
+# 
+# 
+
 # covid: mark obvious disinfo subreddits and broad anti corona regulation subs
 plot <- data %>%
   mutate(misinfo = if_else(
@@ -83,12 +112,12 @@ plot <- data %>%
   ggplot(aes(x = reorder(sub_title,sub_count), y = sub_count, color = misinfo)) +
   geom_count()  +
   coord_flip() +
-  scale_y_log10() 
+  scale_y_log10() #I think this one is worth not doing a logarithmic function, just to show how far the "other" subreddits trail behind the misinfo/anti-reg.
 plot
 
 ##### WORD COUNTING
 # politician mentioned!
-## steps: get stopwod list of all english words, eliminate these from posts, 
+## steps: get stopword list of all english words, eliminate these from posts, 
 ##        look through remaining data and filter out politicians
 ## OR:    make list of relevant politicians and search for them
 
@@ -103,7 +132,8 @@ install.packages("wordcloud")
 install.packages("RColorBrewer")
 install.packages("wordcloud2")
 install.packages("tm")
-
+install.packages("NLP")
+library(NLP)
 library(tm)
 library(tokenizers)
 library(tidyverse)
@@ -113,11 +143,12 @@ library(hunspell)
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
-
+??snowball
 
 extr_subs <- data_grouped #%>%  # <<<<<<<<<<<<<<<<<<<<<<<< maybe more interesting if not only extreme right subreddits
   #distinct(data.body) %>%
   #filter(sub_groups == "source subs" | sub_groups == "extreme right")
+
 
 extr_token <- tokenize_words(extr_subs$data.body, 
                              lowercase = TRUE,
@@ -135,10 +166,61 @@ extr_words$words <- gsub("[\r\n]", "", extr_words$words)
 extr_words$words <- gsub("[[:punct:]]", "", extr_words$words)
 extr_words$words <- gsub("[0123456789]", "", extr_words$words)
 
+# prior to removing meaningless words, maybe a sentiment analysis 
+# comparison within users' posts between the sentiments of their posts on extreme subreddits 
+# vs the other, unrelated subreddits? 
+#
+# script for sentiment analysis
+install.packages("textdata")
+library(textdata)
+
+nrc_sentiments <- get_sentiments("nrc") %>%
+  select(word, sentiment)
+
+#preparing for being able to use inner_join (I think you need same column names (word))
+extr_words_t <- extr_words %>%
+  mutate(word = words)
+
+#inner join and sentiment analysis of words
+subreddit_sentiments <- extr_words_t %>% 
+  inner_join(nrc_sentiments, by = "word") %>%
+  select(data.subreddit, data.author, word, sentiment)
+
+# need to count and compare the frequencies of each sentiment for 
+#two categories: extreme vs other subreddits
+#this would tell us something about how the engagement of these politically extreme users 
+# compare sentiments between their extreme subreddits and their outside subreddits
+# need to use variable with the two categories separated:
+
+#Frequency of subreddit group sentiments <<<< recommend going with this: better potential for visualization, fewer NA fields
+subgroup_sentiment_count <- extr_words_t %>%
+  inner_join(nrc_sentiments, by = "word") %>%
+  count(sub_groups, sentiment) %>%
+  spread(sub_groups, n)
+
+subgroup_sentiment_count
+#NOTE: just also need to rerun the script with a few additional newly categorized subreddits (see updates to script)
+
+# probably also need to turn this into a proportion for better comparison
+# subgroup_sentiment_count %>%
+ # mutate = 
+ # not sure how to code this part^
+
+#Frequency of subreddit sentiments
+sub_sentiment_count <- extr_words_t %>%
+  inner_join(nrc_sentiments, by = "word") %>%
+  count(data.subreddit, sentiment) %>% 
+  spread(data.subreddit, n)
+  
+sub_sentiment_count
+
+  #summarize the overall sentiment of posts and users
+# perhaps we could also do a word cloud for sentiments?
+
 
 extr_words <- extr_words %>%
   filter(!extr_words$words=="",
-         !str_detect(words, "^its$|^dont$|^2$|^day$|^im$|^thats$"),   #<<<<<<<<<<<<<<< DELETE USELESS WORDS
+         !str_detect(words, "^its$|^dont$|^2$|^day$|^im$|^thats$|^yeah$|^lol$|^lot$|^youre$"),   #<<<<<<<<<<<<<<< DELETE USELESS WORDS #added more words to delete here: yeah, lol, lot, youre
          !extr_words$words=="people") ##probably should not delete this 
   
 extr_count <- extr_words %>% 
